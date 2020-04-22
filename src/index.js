efd22b68b86177cc07bd255a1a45fc8b07d34de8
An2 <an2m001@gmail.com>
2020-04-22 00:30:23 -0500

    start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./components/app.js";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/global.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
